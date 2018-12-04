08b6f35ef3b514d1d389d9339170ad9a64f02a60
MoniqueB13 <43286044+MoniqueB13@users.noreply.github.com>
2018-12-04 16:20:22 -0500

    Add files via upload

---
+++ FINAL ASSIGNMENT/page1.js
@@ -0,0 +1,4 @@
+setTimeout(function(){
+  var myAlert = alert("Welcome to Monique's website, hope you enjoy! Have a great day :)");
+  console.log(myAlert);
+}, 3000);
---
+++ FINAL ASSIGNMENT/page2.js
@@ -0,0 +1,4 @@
+setTimeout(function(){
+  var myAlert = alert("Welcome to Monique's website animals page, hope you enjoy! Have a great day :)");
+  console.log(myAlert);
+}, 3000);
